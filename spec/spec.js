a98a6ed8592289ab677de2fe5b9dae5c2258be40
ErikAbrahamson <e.a.abrahamson@gmail.com>
2015-08-04 10:56:13 -0600

    More Jasmine practice

--- spec/spec.js
+++ spec/spec.js
@@ -1,7 +1,14 @@
 var code = require('./../app.js');
 
-describe('', function() {
-  it('', function() {
+describe('changeToUpper', function() {
+  it('returns uppercase items in array', function() {
+    expect(code.changeToUpper(['a','b'])).toEqual(['A','B']);
+  });
+});
 
+describe('getColors', function() {
+  it('should give the color value', function() {
+    var stuff = [ { name: 'Ted', color: 'Red' }, { name: 'Rob', color: 'Blue' } ];
+    expect(code.getColors(stuff)).toEqual('Red');
   });
 });
